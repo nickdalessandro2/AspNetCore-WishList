c940ea21b0aef3bdf3a718aff9e583203fc8524d
nickdalessandro2 <nickdalessandro@att.net>
2022-06-22 13:03:04 -0400

    Created home views and the homecontroller

---
+++ WishList/Controller/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Controller
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+        public IActionResult Error()
+        {
+            return View("Error");
+        }
+
+    }
+}
