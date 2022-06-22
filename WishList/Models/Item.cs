75421283e323b4b8be66d3c37c161401eb94e561
nickdalessandro2 <nickdalessandro@att.net>
2022-06-22 13:19:44 -0400

    Created item model with entity framework support

---
+++ WishList/Models/Item.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+    }
+}
