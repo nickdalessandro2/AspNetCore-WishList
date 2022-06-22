d95e972a7a6b95a9c5c4bb8ebf8ca64b14352836
nickdalessandro2 <nickdalessandro@att.net>
2022-06-22 13:20:49 -0400

    Fixed

--- WishList/Models/Item.cs
+++ WishList/Models/Item.cs
@@ -5,6 +5,9 @@ namespace WishList.Models
     public class Item
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string Description { get; set; }
     }
 }
