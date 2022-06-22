48573af2d02ffdebba4010e87615b18a7fdcd3bb
nickdalessandro2 <nickdalessandro@att.net>
2022-06-22 13:52:57 -0400

    a

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+    }
+}
